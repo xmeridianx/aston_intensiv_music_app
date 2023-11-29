e78fda14bb7a985385be5dcfdc656c77d1ecf53e
Timur Shabaev <119599434+xmeridianx@users.noreply.github.com>
2023-11-29 16:44:11 +0300

    initial commit

---
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.musicapp
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.IBinder
+import android.widget.SeekBar
+import com.example.musicapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var musicService: MusicService
+    private var musicBound = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as MusicService.MusicBinder
+
+            musicService = binder.getService()
+            musicBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            musicBound = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent(this, MusicService::class.java)
+        startService(intent)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
+
+        binding.seekBar.progress = 0
+
+        binding.buttonPlay.setOnClickListener {
+            if (musicBound) {
+                musicService.play()
+            }
+        }
+        binding.buttonPause.setOnClickListener {
+            if (musicBound) {
+                musicService.pause()
+            }
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
+                if (changed) {
+                    if (musicBound) {
+                        musicService.seekTo(pos)
+                    }
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        if (musicBound) {
+            unbindService(connection)
+            musicBound = false
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/musicapp/MusicService.kt
@@ -0,0 +1,157 @@
+package com.example.musicapp
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+
+class MusicService: Service() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private val binder = MusicBinder()
+    private var trackList: List<Int> = listOf(R.raw.music1, R.raw.music2)
+    private var currentTrackIndex = 0
+
+
+    inner class MusicBinder : Binder() {
+        fun getService(): MusicService = this@MusicService
+    }
+    override fun onBind(intent: Intent?): IBinder? {
+        return binder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this, trackList[currentTrackIndex])
+        mediaPlayer.setOnCompletionListener {
+            playNextTrack()
+            playPreviousTrack()
+        }
+    }
+
+    private val NOTIFICATION_ID = 1
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            "PREVIOUS" -> playPreviousTrack()
+            "PLAY_PAUSE" -> togglePlayPause()
+            "NEXT" -> playNextTrack()
+        }
+        updateNotification()
+        return START_STICKY
+    }
+
+    private fun createNotification(): Notification? {
+
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("music_channel", "Music Channel")
+            } else {
+                ""
+            }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.music_logo)
+            .setContentTitle("Сейчас играет")
+            .setContentText("Calvin Harris feat. Florence Welch - Sweet Nothing")
+            .setContentIntent(pendingIntent)
+            .setOngoing(true)
+            .addAction(R.drawable.baseline_arrow_back_ios_24, "Пред.", null)
+            .addAction(R.drawable.baseline_pause_24, "Пауза", null)
+            .addAction(R.drawable.baseline_arrow_forward_ios_24, "След.", null)
+
+        return notificationBuilder.build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val channel = NotificationChannel(
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_LOW
+        )
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+
+        return channelId
+    }
+
+    private fun playNextTrack() {
+        if (currentTrackIndex < trackList.size - 1) {
+            currentTrackIndex++
+        } else {
+            currentTrackIndex = 0
+        }
+        mediaPlayer.reset()
+        mediaPlayer.setDataSource(applicationContext, Uri.parse("android.resource://${packageName}/${trackList[currentTrackIndex]}"))
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+        updateNotification()
+    }
+
+    private fun playPreviousTrack() {
+        if (currentTrackIndex > 0) {
+            currentTrackIndex--
+        } else {
+            currentTrackIndex = trackList.size - 1
+        }
+        mediaPlayer.reset()
+        mediaPlayer.setDataSource(applicationContext, Uri.parse("android.resource://${packageName}/${trackList[currentTrackIndex]}"))
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+        updateNotification()
+    }
+
+    private fun togglePlayPause() {
+        if (mediaPlayer.isPlaying) {
+            pause()
+        } else {
+            play()
+        }
+    }
+
+    fun seekTo(position: Int) {
+        mediaPlayer.seekTo(position)
+    }
+
+    private fun updateNotification() {
+        val notification = createNotification()
+        startForeground(NOTIFICATION_ID, notification)
+    }
+
+    fun play() {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+            updateNotification()
+        }
+    }
+
+    fun pause() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            updateNotification()
+        }
+    }
+
+    override fun onDestroy() {
+        mediaPlayer.release()
+        super.onDestroy()
+    }
+}
